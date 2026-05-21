51a6a3f6e86b9b8e850189041cab073c36f5f45f
IMMANUVEL-WEB <immans358@gmail.com>
2026-05-21 13:44:04 +0530

    added frontend prod url to cors

--- Backend/main.py
+++ Backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://todo-app-frontend-ntr8.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
